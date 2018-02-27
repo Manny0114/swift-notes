e39c8473381d0467a05176e6113e15e316e0ae17
Manuel Soto <sotomanuel41@gmail.com>
2018-02-27 12:29:10 -0600

    Initial Commit

---
+++ swift-notes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  swift-notes
+//
+//  Created by Manuel Soto on 2/27/18.
+//  Copyright © 2018 Manuel Soto. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ swift-notes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swift-notes
+//
+//  Created by Manuel Soto on 2/27/18.
+//  Copyright © 2018 Manuel Soto. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
